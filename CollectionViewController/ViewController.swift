cd7d08ffb60fccc5350c2855647b768e6b605df2
KyooBaek Lee <charisma1124@gmail.com>
2018-08-06 11:34:33 +0900

    why so serious
    why so serious

---
+++ CollectionViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CollectionViewController
+//
+//  Created by CAUAD06 on 2018. 8. 6..
+//  Copyright © 2018년 CAUAD06. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
